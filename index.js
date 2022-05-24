06f20bb408276a3d7c18ccca834c59505e473840
unknown <md616264@gmail.com>
2022-05-24 09:33:49 +0600

    start jwt

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
+require("dotenv").config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 
 app.use(cors())
-app.use(express())
+app.use(express.json())
 
 // assignment12
 // G7YlvVSoSB0K2HoS
@@ -20,8 +22,11 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run() {
     try {
         await client.connect();
-        console.log('db connect');
         const servicesCollection = client.db("assignment12").collection("services");
+        const bookingCollection = client.db("assignment12").collection("booking");
+        const userCollection = client.db("assignment12").collection("users");
+
+
 
         app.get('/services', async (req, res) => {
             const query = {};
@@ -29,18 +34,61 @@ async function run() {
             const services = await cursor.toArray();
             res.send(services)
         });
+
+        app.put('/user/:email', async (req, res) => {
+            const email = req.params.email;
+            const user = req.body;
+            const filter = { email: email };
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: user
+            };
+            const result = await userCollection.updateOne(filter, updateDoc, options);
+            const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1d' })
+            res.send({ result, token });
+        })
         app.get('/services/:id', async (req, res) => {
-            const id = req.params.id;
+            const id = req.params?.id;
             const query = { _id: ObjectId(id) };
             const result = await servicesCollection.findOne(query);
             res.send(result)
+        });
+
+
+        app.get('/booking', async (req, res) => {
+            const query = {};
+            const cursor = bookingCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services)
         })
+        app.post('/booking', async (req, res) => {
+            const booking = req.body;
+            console.log(booking);
+            const result = await bookingCollection.insertOne(booking);
+            res.send(result);
+        });
+
+        app.get('/order/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email: email };
+            const orders = await bookingCollection.find(query).toArray();
+            res.send(orders);
+        });
+
+        app.delete('/delete/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await bookingCollection.deleteOne(query);
+            res.send(result);
+        });
+
+
     }
     finally {
 
     }
 }
-run().catch(console.dir())
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send('Hello server is Running')
